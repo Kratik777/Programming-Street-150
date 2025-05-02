d9b056c1b8344aa9db5c2ba11ec9d5a34684f6c1
iamkratik7 <kratikbhatt7@gmail.com>
2025-05-02 16:40:23 +0530

    solved

--- Sprint 1/Easy/EvenOdd.cpp
+++ Sprint 1/Easy/EvenOdd.cpp
@@ -3,7 +3,14 @@ using namespace std;
 
 int main(){
 
- cout<<"hi";
- 
+ int num1;
+ cin>>num1;
+ if(num1%2 == 0 ){
+    cout<<num1<<" is an Even Number"<<endl;
+ }
+ else{
+    cout<<num1<<" is an Odd Number"<<endl;
+ }
+ return 0;
 
 }
