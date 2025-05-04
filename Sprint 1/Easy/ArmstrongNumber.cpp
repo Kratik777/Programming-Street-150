f1669f4c120ef764f2503e3bb6367c44dd0695ed
Kratik777 <kratikbhatt6@gmail.com>
2025-05-04 10:36:51 +0530

    Problem solved

--- Sprint 1/Easy/ArmstrongNumber.cpp
+++ Sprint 1/Easy/ArmstrongNumber.cpp
@@ -1,6 +1,29 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 
 int main(){
-    cout<<"hii";
+    int num ;
+    cin>>num;
+    int n=0;
+    int rem;
+    int temp = num;
+    int count=0;
+
+    while(temp!=0){
+      int digit = temp%10;
+      count++;
+      temp= temp/10;
+    }
+     temp = num;
+    while(temp!=0){
+      rem = temp%10;
+      n = n + pow(rem , count);
+      temp =  temp/10;
+           
+    }
+    if(n == num){
+      cout<<"Armstrong Number";
+    }
+    return 0;
 }
