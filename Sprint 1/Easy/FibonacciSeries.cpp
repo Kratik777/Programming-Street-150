13b2e5c75c0e26ef139d794cc37a582ebc048349
Kratik777 <kratikbhatt6@gmail.com>
2025-05-05 12:08:45 +0530

    Updates

--- Sprint 1/Easy/FibonacciSeries.cpp
+++ Sprint 1/Easy/FibonacciSeries.cpp
@@ -6,12 +6,11 @@ int main() {
     cin>>limit;
     int n1 = 0;
     int n2 =1;
-    cout<<n1<<" ";
-    cout<<n2<<" ";
-    while(n2+n1<=limit){
+ 
+    while(n1<=limit){
      
+        cout<<n1<<" ";
        int next = n1+n2;
-        cout<<next<<" ";
         n1 = n2;
         n2 = next;
        
---
+++ Sprint 1/Easy/GCD.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    cin>>a>>b;
+    int large;
+    int small;
+    if(a-b>=0){
+        large=a;
+        small=b;
+    }
+    else{
+        large=b;
+        small=a;
+    }
+    int sub=0;
+    sub = large-small;
+    while(a!=b){
+        if(sub>small){
+            a=sub;
+            b=small;
+            sub = sub-small;
+        }
+        else{
+            a=small;
+            b=sub;
+            sub = small-sub;
+        }
+        
+    }
+    cout<<"GCD Of "<<a<<" & "<<b<<" is "<<sub;
+    return 0;
+}
