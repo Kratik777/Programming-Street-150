04bc7570519237aff18963f78f31ba3767d50f3f
Kratik777 <kratikbhatt6@gmail.com>
2025-05-17 12:55:25 +0530

    Problem solved

--- Sprint 2/Medium/SumOfNFibonacciNumbers.cpp
+++ Sprint 2/Medium/SumOfNFibonacciNumbers.cpp
@@ -3,20 +3,19 @@ using namespace std;
 
 int main() {
     int n;
-    cin>>n;
+    cin >> n;
+
     int n1 = 0;
     int n2 = 1;
-    int count = 0;
-    int sum =0;
-    while(count < n){
-  
-        int next = n1+n2;
-        sum = n1+n2;
+    int sum = 0;
+
+    for (int count = 0; count < n; count++) {
+        sum += n1;         
+        int next = n1 + n2;
         n1 = n2;
         n2 = next;
-        count++;
-
     }
-    cout<<sum+1;
+
+    cout << sum;
     return 0;
 }
