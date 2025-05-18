89628646fb8daabb0e87a61c75ee7ee38c0a6490
Kratik777 <kratikbhatt6@gmail.com>
2025-05-18 17:00:33 +0530

    Problem solved

--- Sprint 2/Medium/NumberOfPerfectNumbersUptoN.cpp
+++ Sprint 2/Medium/NumberOfPerfectNumbersUptoN.cpp
@@ -4,5 +4,19 @@ using namespace std;
 int main() {
     int n;
     cin>>n;
+    int count = 0 ;
+    for(int index = 2 ; index <= n; index++){
+        int sum = 0 ; 
+
+        for(int itr = 1; itr < index; itr++){
+            if(index%itr == 0 ){
+                sum = sum+itr;
+            }
+        }
+        if(sum == index){
+          count++;
+        }
+    }
+    cout<<count;
     return 0;
 }
