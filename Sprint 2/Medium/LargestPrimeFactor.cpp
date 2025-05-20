5ba7ab8a03a3212f9adc40a4289ae950d18cd6d9
Kratik777 <kratikbhatt6@gmail.com>
2025-05-20 16:02:04 +0530

    Problem solved

--- Sprint 2/Medium/LargestPrimeFactor.cpp
+++ Sprint 2/Medium/LargestPrimeFactor.cpp
@@ -4,5 +4,25 @@ using namespace std;
 int main() {
     int n;
     cin>>n;
+    int divisor = 1 ;
+    for(int index = 2; index <= n; index++){
+        int count = 0;
+        for(int itr = 2 ; itr < index; itr++){
+            if(index%itr ==0){
+                count = 1;
+                break;
+            }
+            else{
+                continue;
+            }
+
+        }
+        if(count == 0 && n % index == 0 ){
+            if(index > divisor){
+                divisor = index;
+            }
+        }
+    }
+    cout<<divisor<<endl;
     return 0;
 }
