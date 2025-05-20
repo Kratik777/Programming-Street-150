2150f0874956d53a63327d9aa42104e50dc56645
Kratik777 <kratikbhatt6@gmail.com>
2025-05-20 17:16:55 +0530

    Problem solved

--- Sprint 2/Medium/SumOfFirstNPrimeNumbers.cpp
+++ Sprint 2/Medium/SumOfFirstNPrimeNumbers.cpp
@@ -2,6 +2,27 @@
 using namespace std;
 
 int main() {
-    
+    int n;
+    cin>>n;
+    int count = 0 ; 
+    int num = 2;
+    int sum = 0;
+    while(count < n){
+    int flag = 0;
+
+        for(int index = 2; index < num ; index++ ){
+            if(num%index == 0){
+                flag = 1;
+                break;
+            }
+                     
+        }
+        if(flag == 0){
+            sum+=num;
+            count++;
+        }
+        num++;
+    }
+    cout<<sum;
     return 0;
 }
