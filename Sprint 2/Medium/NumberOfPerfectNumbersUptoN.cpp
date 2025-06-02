1549116472e245c5a2618529eb55c079a6b27008
Kratik777 <kratikbhatt6@gmail.com>
2025-06-02 14:46:20 +0530

    Function updated

--- Sprint 2/Medium/NumberOfPerfectNumbersUptoN.cpp
+++ Sprint 2/Medium/NumberOfPerfectNumbersUptoN.cpp
@@ -4,7 +4,19 @@ using namespace std;
 // Function to count perfect numbers upto N.
 int countPerfectNumbers(int N)
 {
-        
+    int count = 0;
+    for(int index = 2 ; index <= N ; index++ ){
+        int sum = 0;
+        for(int itr = 1 ; itr < index ; itr++ ){
+            if(index%itr == 0 ){
+                sum+=itr;
+            }
+        }
+        if(sum == index){
+            count++;
+        }
+    }
+    return count;
 }
 
 int main()
